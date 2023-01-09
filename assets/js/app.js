928705c298a9ad0c580159bffa4ada45411baf14
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2023-01-09 21:32:00 +0700

    done tablet responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+//toggle top-header
 window.onscroll = () => {
     const header = document.querySelector('.header');
     const headerTop = document.querySelector('.header-top');
@@ -11,6 +12,8 @@ window.onscroll = () => {
     }
 };
 
+
+//slides animation
 let slideIndex = 0;
 showSlides();
 
@@ -24,4 +27,36 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+//current section
+
+window.addEventListener("load", (event) => {
+  //auto active first section
+  document.querySelector(".nav-right a[href*=services]").classList.add("active");
+});
+
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("div[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 18;
+    sectionId = current.getAttribute("id");
+    
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector(".nav-right a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".nav-right a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
 }
